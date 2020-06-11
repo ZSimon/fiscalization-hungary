df6b5122b2c7277c11e639f33755a7b68e2fea4e
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-06-11 16:21:26 +0200

    self

---
+++ src/Mews.Fiscalization.Hungary/Models/TokenExchange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mews.Fiscalization.Hungary.Models
+{
+    public sealed class TokenExchange
+    {
+        public TokenExchange(string value, DateTime validFrom, DateTime validTo)
+        {
+            Value = value;
+            ValidFrom = validFrom;
+            ValidTo = validTo;
+        }
+
+        public string Value { get; }
+
+        public DateTime ValidFrom { get; }
+
+        public DateTime ValidTo { get; }
+
+        internal static TokenExchange Map(Dto.TokenExchangeResponse response)
+        {
+            return new TokenExchange(
+                value: response.EncodedExchangeToken,
+                validFrom: response.TokenValidityFrom,
+                validTo: response.TokenValidityTo
+            );
+        }
+    }
+}
---
+++ src/Mews.Fiscalization.Hungary/Utils/Aes.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace Mews.Fiscalization.Hungary.Utils
+{
+    internal static class Aes
+    {
+        public static string Decrypt(string key, byte[] data)
+        {
+            var cipher = CipherUtilities.GetCipher("AES");
+            cipher.Init(false, new KeyParameter(Encoding.UTF8.GetBytes(key)));
+            byte[] final = cipher.DoFinal(data);
+            return Encoding.UTF8.GetString(final);
+        }
+    }
+}
