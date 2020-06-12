2a6a446d9f493596ca03dfa515d9c4d25a9cc1ef
Pavel Kalandra <kalandrapavel@gmail.com>
2020-06-12 16:54:47 +0200

    Extracted network communication to a separate class. (#8)

---
+++ src/Mews.Fiscalization.Hungary/Client.cs
@@ -0,0 +1,58 @@
+﻿using Mews.Fiscalization.Hungary.Models;
+using Mews.Fiscalization.Hungary.Models.TaxPayer;
+using Mews.Fiscalization.Hungary.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mews.Fiscalization.Hungary
+{
+    internal class Client
+    {
+        private HttpClient HttpClient { get; }
+        private NavEnvironment Environment { get; }
+
+        internal Client(HttpClient httpClient, NavEnvironment environment)
+        {
+            HttpClient = httpClient;
+            Environment = environment;
+        }
+
+        internal async Task<ResponseResult<TResult>> ProcessRequestAsync<TRequest, TDto, TResult>(string endpoint, TRequest request, Func<TDto, ResponseResult<TResult>> successFunc)
+            where TRequest : class
+            where TDto : class
+            where TResult : class
+        {
+            var httpResponse = await SendRequestAsync(endpoint, request);
+            return await DeserializeAsync(httpResponse, successFunc);
+        }
+
+        private async Task<HttpResponseMessage> SendRequestAsync<TRequest>(string endpoint, TRequest request)
+            where TRequest : class
+        {
+            var content = new StringContent(XmlManipulator.Serialize(request), Encoding.UTF8, "application/xml");
+            var uri = new Uri(ServiceInfo.BaseUrls[Environment], $"{ServiceInfo.RelativeServiceUrl}{endpoint}");
+            return await HttpClient.PostAsync(uri, content);
+        }
+
+        private async Task<ResponseResult<TResult>> DeserializeAsync<TDto, TResult>(HttpResponseMessage response, Func<TDto, ResponseResult<TResult>> successFunc)
+            where TDto : class
+            where TResult : class
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                return successFunc(XmlManipulator.Deserialize<TDto>(content));
+            }
+            else
+            {
+                var errorResult = XmlManipulator.Deserialize<Dto.GeneralErrorResponse>(content);
+                return new ResponseResult<TResult>(errorResult: ErrorResult.Map(errorResult));
+            }
+        }
+    }
+}
--- src/Mews.Fiscalization.Hungary/NavClient.cs
+++ src/Mews.Fiscalization.Hungary/NavClient.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace Mews.Fiscalization.Hungary
 
         private static HttpClient HttpClient { get; }
 
-        private NavEnvironment Environment { get; }
+        private Client Client { get; }
 
         static NavClient()
         {
@@ -30,63 +31,13 @@ namespace Mews.Fiscalization.Hungary
         {
             TechnicalUser = technicalUser;
             SoftwareIdentification = softwareIdentification;
-            Environment = environment;
-        }
-
-        public async Task<ResponseResult<string>> SendInvoicesAsync(ExchangeToken token, IEnumerable<InvoiceData> invoices)
-        {
-            // TODO: Handle additionalSignatureData
-            var request = CreateRequest<Dto.ManageInvoiceRequest>();
-            request.exchangeToken = Convert.ToBase64String(token.Value);
-            request.invoiceOperations = new Dto.InvoiceOperationListType
-            {
-                compressedContent = false,
-                invoiceOperation = MapToInvoiceOperation(invoices).ToArray()
-            };
-
-            return await ProcessRequestAsync<Dto.ManageInvoiceRequest, Dto.ManageInvoiceResponse, string>("manageInvoice", request, response =>
-            {
-                return new ResponseResult<string>(successResult: response.transactionId);
-            });
-        }
-
-        private IEnumerable<Dto.InvoiceOperationType> MapToInvoiceOperation(IEnumerable<InvoiceData> invoices)
-        {
-            var operations = new List<Dto.InvoiceOperationType>();
-            for (int i = 1; i <= invoices.Count(); ++i)
-            {
-                var invoice = invoices.ElementAt(i);
-                operations.Add(new Dto.InvoiceOperationType
-                {
-                    index = i,
-                    invoiceData = Encoding.UTF8.GetBytes(MapInvoiceData(invoice).ToString()), // TODO: invoice data serialized and then converted to base64.
-                    invoiceOperation = Dto.ManageInvoiceOperationType.CREATE // For now, we will only support create operations.
-                });
-            }
-
-            return operations;
-        }
-
-        private Dto.InvoiceData MapInvoiceData(InvoiceData invoice)
-        {
-            return new Dto.InvoiceData
-            {
-                invoiceIssueDate = invoice.InvoiceIssueDate,
-                invoiceMain = new Dto.InvoiceMainType
-                {
-                    Items = new object[]
-                    {
-                        // TODO: fill in with Invoice items.
-                    }
-                },
-                invoiceNumber = invoice.InvoiceNumber
-            };
+            Client = new Client(HttpClient, environment);
         }
 
         public async Task<ResponseResult<ExchangeToken>> GetExchangeTokenAsync()
         {
             var request = CreateRequest<Dto.TokenExchangeRequest>();
-            return await ProcessRequestAsync<Dto.TokenExchangeRequest, Dto.TokenExchangeResponse, ExchangeToken>("tokenExchange", request, response =>
+            return await Client.ProcessRequestAsync<Dto.TokenExchangeRequest, Dto.TokenExchangeResponse, ExchangeToken>("tokenExchange", request, response =>
             {
                 var decryptedToken = Aes.Decrypt(TechnicalUser.EncryptionKey, response.encodedExchangeToken);
                 return new ResponseResult<ExchangeToken>(successResult: new ExchangeToken(
@@ -101,7 +52,7 @@ namespace Mews.Fiscalization.Hungary
         {
             var request = CreateRequest<Dto.QueryTaxpayerRequest>();
             request.taxNumber = taxNumber;
-            return await ProcessRequestAsync<Dto.QueryTaxpayerRequest, Dto.QueryTaxpayerResponse, TaxPayerData>("queryTaxpayer", request, response =>
+            return await Client.ProcessRequestAsync<Dto.QueryTaxpayerRequest, Dto.QueryTaxpayerResponse, TaxPayerData>("queryTaxpayer", request, response =>
             {
                 if (response.taxpayerValidity) // TODO - it's nullable
                 {
@@ -111,6 +62,47 @@ namespace Mews.Fiscalization.Hungary
                 {
                     return new ResponseResult<TaxPayerData>(errorResult: new ErrorResult("Invalid tax payer.", ResultErrorCode.InvalidTaxPayer));
                 }
+            });
+        }
+
+        public async Task<ResponseResult<string>> SendInvoicesAsync(ExchangeToken token, IEnumerable<InvoiceData> invoices)
+        {
+            // TODO: Handle additionalSignatureData
+            var request = CreateRequest<Dto.ManageInvoiceRequest>();
+
+
+            var invoiceOperations = invoices.Select((invoice, i) =>
+            {
+                var invoiceData = new Dto.InvoiceData
+                {
+                    invoiceNumber = invoice.InvoiceNumber,
+                    invoiceIssueDate = invoice.InvoiceIssueDate,
+                    invoiceMain = new Dto.InvoiceMainType
+                    {
+                        Items = new object[]
+                        {
+                            // TODO: fill in with Invoice items.
+                        }
+                    }
+                };
+                return new Dto.InvoiceOperationType
+                {
+                    index = i + 1,
+                    invoiceData = Encoding.UTF8.GetBytes(invoiceData.ToString()), // TODO: invoice data serialized and then converted to base64.
+                    invoiceOperation = Dto.ManageInvoiceOperationType.CREATE // For now, we will only support create operations.
+                };
+            });
+
+            request.exchangeToken = Convert.ToBase64String(token.Value);
+            request.invoiceOperations = new Dto.InvoiceOperationListType
+            {
+                compressedContent = false,
+                invoiceOperation = invoiceOperations.ToArray()
+            };
+
+            return await Client.ProcessRequestAsync<Dto.ManageInvoiceRequest, Dto.ManageInvoiceResponse, string>("manageInvoice", request, response =>
+            {
+                return new ResponseResult<string>(successResult: response.transactionId);
             });
         }
 
@@ -154,39 +146,6 @@ namespace Mews.Fiscalization.Hungary
             var formattedTimestamp = timestamp.ToString("yyyyMMddHHmmss");
             var signatureData = $"{requestId}{formattedTimestamp}{user.XmlSigningKey}{additionalSignatureData}";
             return Sha512.GetSha3Hash(signatureData);
-        }
-
-        private async Task<ResponseResult<TResult>> ProcessRequestAsync<TRequest, TDto, TResult>(string endpoint, TRequest request, Func<TDto, ResponseResult<TResult>> successFunc)
-            where TRequest : class
-            where TDto : class
-            where TResult : class
-        {
-            var httpResponse = await SendRequestAsync(endpoint, request);
-            return await DeserializeAsync(httpResponse, successFunc);
-        }
-
-        private async Task<HttpResponseMessage> SendRequestAsync<TRequest>(string endpoint, TRequest request)
-            where TRequest : class
-        {
-            var content = new StringContent(XmlManipulator.Serialize(request), Encoding.UTF8, "application/xml");
-            var uri = new Uri(ServiceInfo.BaseUrls[Environment], $"{ServiceInfo.RelativeServiceUrl}{endpoint}");
-            return await HttpClient.PostAsync(uri, content);
-        }
-
-        private async Task<ResponseResult<TResult>> DeserializeAsync<TDto, TResult>(HttpResponseMessage response, Func<TDto, ResponseResult<TResult>> successFunc)
-            where TDto : class
-            where TResult : class
-        {
-            var content = await response.Content.ReadAsStringAsync();
-            if (response.IsSuccessStatusCode)
-            {
-                return successFunc(XmlManipulator.Deserialize<TDto>(content));
-            }
-            else
-            {
-                var errorResult = XmlManipulator.Deserialize<Dto.GeneralErrorResponse>(content);
-                return new ResponseResult<TResult>(errorResult: ErrorResult.Map(errorResult));
-            }
         }
     }
 }
