9fe2de902f52f50769198ac94d9f194a807216fd
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-06-17 18:58:56 +0200

    WIP

--- src/Mews.Fiscalization.Hungary/Models/Invoice/Invoice.cs
+++ src/Mews.Fiscalization.Hungary/Models/Invoice/Invoice.cs
@@ -13,12 +13,9 @@ namespace Mews.Fiscalization.Hungary.Models
             Info supplierInfo,
             Info customerInfo,
             IEnumerable<Item> items,
-            Amount amount,
-            Amount amountHUF,
             DateTime deliveryDate,
             DateTime paymentDate,
             CurrencyCode currencyCode,
-            ExchangeRate exchangeRate,
             bool isSelfBilling = false,
             bool isCashAccounting = false)
         {
@@ -27,12 +24,9 @@ namespace Mews.Fiscalization.Hungary.Models
             SupplierInfo = Check.NotNull(supplierInfo, nameof(supplierInfo));
             CustomerInfo = Check.NotNull(customerInfo, nameof(customerInfo));
             Items = Check.NotNull(items, nameof(items));
-            Amount = Check.NotNull(amount, nameof(amount));
-            AmountHUF = Check.NotNull(amountHUF, nameof(amountHUF));
             DeliveryDate = deliveryDate;
             PaymentDate = paymentDate;
             CurrencyCode = Check.NotNull(currencyCode, nameof(currencyCode));
-            ExchangeRate = Check.NotNull(exchangeRate, nameof(exchangeRate));
             IsSelfBilling = isSelfBilling;
             IsCashAccounting = isCashAccounting;
             TaxSummary = items.GroupBy(i => i.Amounts.TaxRatePercentage).Select(g => new TaxSummaryItem(
@@ -54,17 +48,11 @@ namespace Mews.Fiscalization.Hungary.Models
 
         public IEnumerable<TaxSummaryItem> TaxSummary { get; }
 
-        public Amount Amount { get; }
-
-        public Amount AmountHUF { get; }
-
         public DateTime DeliveryDate { get; }
 
         public DateTime PaymentDate { get; }
 
         public CurrencyCode CurrencyCode { get; }
-
-        public ExchangeRate ExchangeRate { get; }
 
         public bool IsSelfBilling { get; }
 
