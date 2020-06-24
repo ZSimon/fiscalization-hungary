dd1ce5e753cb6db7ce40900cf1cf54cb869e84fd
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-06-24 18:04:57 +0200

    Introduced validation for the encryption key.

---
+++ src/Mews.Fiscalization.Hungary/Models/Types/EncryptionKey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mews.Fiscalization.Hungary.Models.Types
+{
+    public sealed class EncryptionKey : ValidatedString
+    {
+        private static readonly string regexValidation = "^[0-9A-Za-z]{16}$";
+
+        public EncryptionKey(string value)
+            : base(value, 16, 16, regexValidation)
+        {
+        }
+
+        public static bool IsValid(string value)
+        {
+            return IsValid(value, 16, 16, regexValidation);
+        }
+    }
+}
