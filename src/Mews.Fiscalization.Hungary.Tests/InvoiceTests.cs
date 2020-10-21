20e3afd8d8f0519d7ce149843031e7980be52d2c
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2020-10-21 17:28:36 +0200

    Introduced tests and GitHub actions/secrets (#46)
    * Introduced tests and github actions.
    
    * build
    
    * CR
    
    * CR
    
    * CR
    
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/Mews.Fiscalization.Hungary.Tests/InvoiceTests.cs
@@ -0,0 +1,182 @@
+﻿using Mews.Fiscalization.Hungary.Models;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mews.Fiscalization.Hungary.Tests
+{
+    [TestFixture]
+    public class InvoiceTests
+    {
+        [Test]
+        public async Task Test()
+        {
+            var navClient = TestFixture.GetClient();
+            var exchangeToken = await navClient.GetExchangeTokenAsync();
+            var invoiceTransactionId = await navClient.SendInvoicesAsync(exchangeToken.SuccessResult, SequentialEnumerable.FromPreordered(new[] { GetInvoice() }, startIndex: 1));
+
+            Thread.Sleep(3000);
+
+            var transactionStatus = await navClient.GetTransactionStatusAsync(invoiceTransactionId.SuccessResult);
+            Assert.IsNotNull(transactionStatus.SuccessResult);
+            Assert.IsNotNull(exchangeToken.SuccessResult);
+            Assert.IsNotNull(invoiceTransactionId.SuccessResult);
+            AssertError(transactionStatus);
+            AssertError(exchangeToken);
+            AssertError(invoiceTransactionId);
+        }
+
+        [Test]
+        public async Task CorrectionInvoiceSucceeds()
+        {
+            var navClient = TestFixture.GetClient();
+            var exchangeToken = await navClient.GetExchangeTokenAsync();
+            var response = await navClient.SendModificationDocumentsAsync(
+                token: exchangeToken.SuccessResult,
+                invoices: SequentialEnumerable.FromPreordered(new[] { GetModificationInvoice() }, startIndex: 1)
+            );
+
+            Thread.Sleep(3000);
+
+            var transactionStatus = await navClient.GetTransactionStatusAsync(response.SuccessResult);
+            Assert.IsNotNull(transactionStatus.SuccessResult);
+            AssertError(transactionStatus);
+        }
+
+        private Invoice GetInvoice()
+        {
+            var item1Amount = new Amount(new AmountValue(1), new AmountValue(1), new AmountValue(0));
+            var item2Amount = new Amount(new AmountValue(20m), new AmountValue(16.81m), new AmountValue(3.19m));
+            var unitAmount1 = new ItemAmounts(item1Amount, item2Amount, 0.05m);
+            var items = new[]
+            {
+                new InvoiceItem(
+                    consumptionDate: new DateTime(2020, 06, 30),
+                    totalAmounts: new ItemAmounts(item1Amount, item1Amount, 0.05m),
+                    description: new Description("Httt hzi serts (fl)"),
+                    measurementUnit: MeasurementUnit.Night,
+                    quantity: 15,
+                    unitAmounts: unitAmount1,
+                    exchangeRate: new ExchangeRate(1)
+                ),
+                new InvoiceItem(
+                    consumptionDate: new DateTime(2020, 06, 30),
+                    totalAmounts: new ItemAmounts(item2Amount, item2Amount, 0.05m),
+                    description: new Description("Httt hzi serts (fl)"),
+                    measurementUnit: MeasurementUnit.Night,
+                    quantity: -15,
+                    unitAmounts: unitAmount1,
+                    exchangeRate: new ExchangeRate(1)
+                ),
+            };
+
+            var address = GetAddress();
+            return new Invoice(
+                number: new InvoiceNumber("ABC-18a"),
+                issueDate: new DateTime(2020, 06, 30),
+                supplierInfo: GetSupplierInfo(),
+                customerInfo: GetCustomerInfo(),
+                items: SequentialEnumerable.FromPreordered(items, startIndex: 1),
+                paymentDate: new DateTime(2020, 06, 14),
+                currencyCode: new CurrencyCode("EUR")
+            );
+        }
+
+        private ModificationInvoice GetModificationInvoice()
+        {
+            var amounts = GetItemAmounts(amount: 100, exchangeRate: 300);
+            var unitAmounts = GetItemAmounts(amount: 100, exchangeRate: 300);
+            var item = new InvoiceItem(
+                consumptionDate: new DateTime(2020, 08, 30),
+                totalAmounts: amounts,
+                description: new Description("NIGHT 8/30/2020"),
+                measurementUnit: MeasurementUnit.Night,
+                quantity: -1,
+                unitAmounts: unitAmounts,
+                exchangeRate: new ExchangeRate(300)
+            );
+
+            var amounts1 = GetItemAmounts(amount: 100, exchangeRate: 300);
+            var unitAmounts1 = GetItemAmounts(amount: 100, exchangeRate: 300);
+            var item1 = new InvoiceItem(
+                consumptionDate: new DateTime(2020, 08, 31),
+                totalAmounts: amounts1,
+                description: new Description("NIGHT2 8/31/2020"),
+                measurementUnit: MeasurementUnit.Night,
+                quantity: 1,
+                unitAmounts: unitAmounts1,
+                exchangeRate: new ExchangeRate(300)
+            );
+
+            return new ModificationInvoice(
+                number: new InvoiceNumber("ABC-18abfcefsaa"),
+                supplierInfo: GetSupplierInfo(),
+                customerInfo: GetCustomerInfo(),
+                items: SequentialEnumerable.FromPreordered(new[] { item, item1 }, startIndex: 1),
+                currencyCode: new CurrencyCode("USD"),
+                issueDate: new DateTime(2020, 08, 31),
+                paymentDate: new DateTime(2020, 08, 31),
+                itemIndexOffset: 4,
+                modificationIndex: 4,
+                modifyWithoutMaster: true,
+                originalDocumentNumber: new InvoiceNumber("ABC-18afasadafa")
+            );
+        }
+
+        private CustomerInfo GetCustomerInfo()
+        {
+            return new CustomerInfo(
+                taxpayerId: new TaxPayerId("14750636"),
+                name: new Name("Vev Kft"),
+                address: GetAddress()
+            );
+        }
+
+        private SupplierInfo GetSupplierInfo()
+        {
+            return new SupplierInfo(
+                taxpayerId: new TaxPayerId("14750636"),
+                vatCode: new VatCode("2"),
+                name: new Name("BUDAPESTI MSZAKI S GAZDASGTUDOMNYI EGYETEM"),
+                address: GetAddress()
+            );
+        }
+
+        private SimpleAddress GetAddress()
+        {
+            return new SimpleAddress(
+                new City("Budapest"),
+                new CountryCode("HU"),
+                new AdditionalAddressDetail("Test"),
+                new PostalCode("1111")
+            );
+        }
+
+        private ItemAmounts GetItemAmounts(decimal amount, decimal exchangeRate = 1)
+        {
+            var amountHUF = amount * exchangeRate;
+            return new ItemAmounts(
+                amount: new Amount(
+                    net: new AmountValue(amount),
+                    gross: new AmountValue(amount),
+                    tax: new AmountValue(amount)
+                ),
+                amountHUF: new Amount(
+                    net: new AmountValue(amountHUF),
+                    gross: new AmountValue(amountHUF),
+                    tax: new AmountValue(amountHUF)
+                ),
+                taxRatePercentage: 0
+            );
+        }
+
+        private void AssertError<TResult, TCode>(ResponseResult<TResult, TCode> result)
+            where TResult : class
+            where TCode : struct
+        {
+            Assert.IsNull(result.OperationalErrorResult);
+            Assert.IsNull(result.GeneralErrorResult);
+        }
+    }
+}
---
+++ src/Mews.Fiscalization.Hungary.Tests/TestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Mews.Fiscalization.Hungary.Models;
+using System;
+
+namespace Mews.Fiscalization.Hungary.Tests
+{
+    public static class TestFixture
+    {
+        private static readonly Login Login = new Login(Environment.GetEnvironmentVariable("login") ?? "INSERT_LOGIN");
+        private static readonly string Password = Environment.GetEnvironmentVariable("password") ?? "INSERT_PASSWORD";
+        private static readonly SigningKey SigningKey = new SigningKey(Environment.GetEnvironmentVariable("signing_key") ?? "INSERT_SIGNING_KEY");
+        private static readonly TaxPayerId TaxPayerId = new TaxPayerId(Environment.GetEnvironmentVariable("tax_payer_id") ?? "INSERT_TAX_PAYER_ID");
+        private static readonly EncryptionKey EncryptionKey = new EncryptionKey(Environment.GetEnvironmentVariable("encryption_key") ?? "INSERT_ENCRYPTION_KEY");
+
+        public static NavClient GetClient()
+        {
+            var technicalUser = new TechnicalUser(
+                login: Login,
+                password: Password,
+                signingKey: SigningKey,
+                taxId: TaxPayerId,
+                encryptionKey: EncryptionKey
+            );
+            var softwareIdentification = new SoftwareIdentification(
+                id: "123456789123456789",
+                name: "Test",
+                type: SoftwareType.LocalSoftware,
+                mainVersion: "1.0",
+                developerName: "Test",
+                developerContact: "test@test.com"
+            );
+            return new NavClient(technicalUser, softwareIdentification, NavEnvironment.Test);
+        }
+    }
+}
